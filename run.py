71a9f06362d7b15063035a9aaf1b25b6391861eb
smartwa <smartwacaleb@gmail.com>
2024-04-29 02:27:49 +0300

    feat: Add start.sh script

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from dotenv import load_dotenv
 from pathlib import Path
 import os
---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+BOTDIR="pytgpt-bot"
+
+function setup(){
+    if [ -d "$BOTDIR" ]; then
+       cd "$BOTDIR"
+       git checkout master
+       git pull origin master
+    
+    else
+       git clone -b master https://github.com/Simatwa/pytgpt-bot.git "$BOTDIR"
+       cd "$BOTDIR"
+    
+    fi
+    pip install .
+    pip install pymysql
+}
+
+function move_to_path(){
+    # environment variables
+    cp ../.env .
+}
+
+function start_server(){
+    python "run.py"
+}
+
+setup
+move_to_path
+start_server
