e7442a2a0368fccf127402db29a36373eecba25b
Simatwa <smartwacaleb@gmail.com>
2024-11-11 03:09:36 +0300

    feat: update python-tgpt dependency version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     },
     install_requires=[
         "pytelegrambotapi==4.17.0",
-        "python-tgpt==0.7.0",
+        "python-tgpt>=0.8.0",
         "python-dotenv==1.0.0",
         "click==8.1.3",
         "SQLAlchemy==2.0.29",
